6de56fe3f2c885fd5ef855c0773152ff52817bae
Endurancex <shengjie_lix@163.com>
2025-08-15 12:32:16 +0800

    add script

--- XDMA/linux-kernel/xdma_stream/xdma_stream_test.sh
+++ XDMA/linux-kernel/xdma_stream/xdma_stream_test.sh
@@ -1,15 +1,49 @@
 #!/bin/bash
-# 64KB
-transferSz=0x10000
+display_help() {
+	echo "$0 <Channel Count>"
+	echo
+       
+	exit 1
+}
+
+if [ $# -eq 0 ]; then
+	display_help
+fi
+
+
+# 2048MB
+transferSz=0x80000000
 # repeat time
+ch=$1
 transferCount=1
 
 tool_path=../tools
 
-$tool_path/dma_from_device -d /dev/xdma0_c2h_0 -s $transferSz -f data/c2h_data.bin -c $transferCount
+for ((i=0; i<$ch; i++)); do
+    $tool_path/dma_from_device -d /dev/xdma0_c2h_${i} -s $transferSz -f data/c2h_data.bin -c $transferCount &
+done
+
+
+# AXIS Data Gen
+packet_len_addr=0x0
+packet_len=$((transferSz/32))
+statusRegVal=`$tool_path/reg_rw /dev/xdma0_user ${packet_len_addr} w ${packet_len}| grep "Read.*:" | sed 's/Read.*: 0x\([a-z0-9]*\)/\1/'`
+statusRegVal=`$tool_path/reg_rw /dev/xdma0_user ${packet_len_addr} w | grep "Read.*:" | sed 's/Read.*: 0x\([a-z0-9]*\)/\1/'`
+echo $packet_len_addr $statusRegVal
+
+start_addr=0x8
+statusRegVal=`$tool_path/reg_rw /dev/xdma0_user ${start_addr} w 1| grep "Read.*:" | sed 's/Read.*: 0x\([a-z0-9]*\)/\1/'`
+statusRegVal=`$tool_path/reg_rw /dev/xdma0_user ${start_addr} w | grep "Read.*:" | sed 's/Read.*: 0x\([a-z0-9]*\)/\1/'`
+echo $start_addr $statusRegVal
+statusRegVal=`$tool_path/reg_rw /dev/xdma0_user ${start_addr} w 0| grep "Read.*:" | sed 's/Read.*: 0x\([a-z0-9]*\)/\1/'`
+statusRegVal=`$tool_path/reg_rw /dev/xdma0_user ${start_addr} w | grep "Read.*:" | sed 's/Read.*: 0x\([a-z0-9]*\)/\1/'`
+echo $start_addr $statusRegVal
+
 wait
 
-$tool_path/dma_to_device -d /dev/xdma0_h2c_0 -s $transferSz -f data/c2h_data.bin -c $transferCount
+for ((i=0; i<$ch; i++)); do
+    $tool_path/dma_to_device -d /dev/xdma0_h2c_${i} -s $transferSz -f data/c2h_data.bin -c $transferCount &
+done
 wait
 
 exit 0
